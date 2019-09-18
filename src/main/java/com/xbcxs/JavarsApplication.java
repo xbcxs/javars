d502ad1f69d809562a2f87061ca5a8a656fc4a82
xiaosh <xiaosh@sysware.com.cn>
2019-09-18 11:43:40 +0800

    Initial commit

---
+++ src/main/java/com/xbcxs/JavarsApplication.java
@@ -0,0 +1,13 @@
+package com.xbcxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarsApplication.class, args);
+	}
+
+}
